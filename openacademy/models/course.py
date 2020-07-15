9e8936a3205ef5e05681e4bad30510adc5f9fa6d
Alejandro Claros <alejandrocontreras.ac83@gmail.com>
2020-07-15 13:58:06 -0600

    Arbert407

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -8,3 +8,11 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+
+    responsible_id = fields.Many2one('res.users',
+    ondelete='set null', string="Responsible", index=True)
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+    ondelete='cascade', string="Course", required=True)
+
